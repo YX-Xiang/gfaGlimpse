24310c7840075dc5d1427c7772509a1d54233c44
YX-Xiang <997214586@qq.com>
2024-09-19 22:27:22 +0800

    run.sh

--- V0/run.sh
+++ V0/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # INPUT_GFA="../data/test.gfa"
-# INPUT_GFA="../example/01_dm_pggb.gfa"
+INPUT_GFA="../example/01_dm_pggb.gfa"
 # INPUT_GFA="../example/02_dm_mc.gfa"
 # INPUT_GFA="/home/yxxiang/data/gfaGlimpse/benchmark/data/03_at_pggb.gfa"
 # INPUT_GFA="/home/yxxiang/data/gfaGlimpse/benchmark/data/04_at_mc.gfa"
 # INPUT_GFA="/home/yxxiang/data/gfaGlimpse/benchmark/data/05_hs_pggb.gfa"
-INPUT_GFA="/home/yxxiang/data/gfaGlimpse/benchmark/data/07_hs_pggb.gfa"
+# INPUT_GFA="/home/yxxiang/data/gfaGlimpse/benchmark/data/07_hs_pggb.gfa"
 OUTPUT_PATH="../data"
 
 make clean > build.log 2>&1
